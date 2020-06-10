b6419467cb2738080b58847e449eba7d703dcd56
Jeeva-ctrl <jeevabharathy66@gmail.com>
2020-06-11 08:40:59 +1000

    Updates

---
+++ precache-manifest.45ca57da554f465245a43caaf564608c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33f1e22b61d6b0e9f44b567ee760b58",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "501025a54b079eda640f",
+    "url": "/react-calculator-app/static/css/main.a6ec5751.chunk.css"
+  },
+  {
+    "revision": "0fe6085273b136400311",
+    "url": "/react-calculator-app/static/js/2.4ca42993.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator-app/static/js/2.4ca42993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501025a54b079eda640f",
+    "url": "/react-calculator-app/static/js/main.96d92264.chunk.js"
+  },
+  {
+    "revision": "cd8c8420964f9c526b75",
+    "url": "/react-calculator-app/static/js/runtime-main.ba6f0499.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
